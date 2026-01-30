e998362cf00470920a0c6bc97459990e51452152
Adrian Cosio <adriancosio14@gmail.com>
2026-01-30 06:50:46 -0600

    feat(WIP): add custom class for cart pole.

--- src/envs/cartpole_env.py
+++ src/envs/cartpole_env.py
@@ -1,1 +1,28 @@
 # Make my own cartpole environment
+
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+
+
+class MyGridWorld(gym.Env):
+
+    def __init__(self, size: int = 5):
+        self.size = size # grid size 5 x 5
+
+        self.observation_space = spaces.Dict(
+            {
+                "cart_position": spaces.Box(0, size, dtype=float),
+                "cart_velocity": spaces.Box(0, size, dtype=float),
+                "pole_position": spaces.Box(0, size, dtype=float),
+                "pole_angle": spaces.Box(0, size, dtype=float),
+                "pole_angular_velocity": spaces.Box(0, size, dtype=float),
+            }
+        )
+        self._cart_position
+        self._cart_velocity
+        self._pole_position
+        self._pole_angle
+        self._pole_angular_velocity
+
+        self.action_space = spaces.Discrete(2) # Left or right
