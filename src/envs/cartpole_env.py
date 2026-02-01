c37337ae839abc9b440bdecb11499ea7428124cc
Adrian Cosio <adriancosio14@gmail.com>
2026-02-01 06:42:09 -0600

    WIP: modify init variables based on physics contstant, add equations of motion

--- src/envs/cartpole_env.py
+++ src/envs/cartpole_env.py
@@ -7,34 +7,46 @@ from gymnasium import spaces
 
 class CartPoleEnv(gym.Env):
 
-    def __init__(self, size: int = 5):
-        self.size = size 
+    def __init__(self):
+        
+        # physical constants
+        self.gravity = 9.8 # m/s/s
+        self.cart_mass = 10 # kg
+        self.pole_mass = 5
+        self.pole_length = 3
 
+        # initalized variables
+        self._cart_position = 0.0
+        self._cart_velocity = 0.0
+        self._cart_acceleration = 0.0
+        self._pole_angle = 0.0
+        self._pole_angular_velocity = 0.0
+        self._pole_angular_acceleration = 0.0
+        self._force = 0.0
+        
+        # observation and action space
         self.observation_space = spaces.Dict(
             {
-                "cart_position": spaces.Box(-size, size, dtype=np.float32),
-                "cart_velocity": spaces.Box(-np.inf, np.inf, dtype=np.float32),
-                "pole_angle": spaces.Box(-np.deg2rad(30), np.deg2rad(30), dtype=np.float32), 
-                "pole_angular_velocity": spaces.Box(-np.inf, np.inf, dtype=np.float32),
+                "cart_position": spaces.Box(-5.0, 5.0, shape=(1,), dtype=np.float32),
+                "cart_velocity": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
+                "cart_acceleration": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float64),
+
+                "pole_angle": spaces.Box(-np.deg2rad(30), np.deg2rad(30), shape=(1,), dtype=np.float32), 
+                "pole_angular_velocity": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
+                "pole_angular_accelerate": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
             }
         )
-        self._cart_position = np.array([0], dtype=np.float32)
-        self._cart_velocity = np.array([0], dtype=np.float32)
-        self._pole_angle = np.array([0], dtype=np.float32)
-        self._pole_angular_velocity = np.array([0], dtype=np.float32)
-
-        self.action_space = spaces.Discrete(2)
-
-        self._force_applied = {
-            0: np.array([-1]),
-            1: np.array([1]),
-        }
+        self.action_space = spaces.Dict(
+            {
+                "force": spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32)
+            }
+        )
 
     def _get_obs(self):
         """Convert internal state to observation format.
 
         Returns:
-            dict: Observation with cart and poles states (position, velocity, angle, ang_rate)
+            dict: Observation with cart and poles states (position, velocity, angle, angular velocity)
         """
         return {
             "cart_position": self._cart_position,
@@ -59,3 +71,15 @@ class CartPoleEnv(gym.Env):
         self._pole_angle = self.np_random.uniform(-np.deg2rad(15), np.deg2rad(15))
 
         return self._get_obs()
+    
+    def step(self):
+
+        x1 = self._get_obs()["cart_position"]
+        x2 = self._get_obs()["cart_velocity"]
+        x3 = self._get_obs()["pole_angle"] # theta
+        x4 = self._get_obs()["pole_angular_velocity"] # theta_dot
+        
+        x1_dot= x2
+        x2_dot = -mp * L * np.sin(x3) * np.square(x4) + mp * g * np.cos(x3) * np.sin(x4) + Force / (M + mp * np.square(np.sin(x3)))
+        x3_dot = x4
+        x4_dot = -(M + mp) * g * np.sin(x3) - mp * L * np.sin(x3) * np.cos(x3) * np.square(x4) - Force * np.cos(x3) / (L * (M + mp * np.square(np.sin(x3))) )
