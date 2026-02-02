10ff01551c622aa3a3a4f576121fdbc34fa4bf15
Adrian Cosio <adriancosio14@gmail.com>
2026-02-02 06:59:07 -0600

    WIP: stopping point. Updtae init function. Modified step funciton

--- src/envs/cartpole_env.py
+++ src/envs/cartpole_env.py
@@ -3,6 +3,7 @@
 import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
+from scipy.integrate import odeint
 
 
 class CartPoleEnv(gym.Env):
@@ -15,32 +16,25 @@ class CartPoleEnv(gym.Env):
         self.pole_mass = 5
         self.pole_length = 3
 
-        # initalized variables
+        # initalized state variables
         self._cart_position = 0.0
         self._cart_velocity = 0.0
-        self._cart_acceleration = 0.0
         self._pole_angle = 0.0
         self._pole_angular_velocity = 0.0
-        self._pole_angular_acceleration = 0.0
-        self._force = 0.0
+
+        # self.numerical_integrator ("original CartPole used: euler")
         
         # observation and action space
         self.observation_space = spaces.Dict(
             {
                 "cart_position": spaces.Box(-5.0, 5.0, shape=(1,), dtype=np.float32),
                 "cart_velocity": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
-                "cart_acceleration": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float64),
 
                 "pole_angle": spaces.Box(-np.deg2rad(30), np.deg2rad(30), shape=(1,), dtype=np.float32), 
                 "pole_angular_velocity": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
-                "pole_angular_accelerate": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
             }
         )
-        self.action_space = spaces.Dict(
-            {
-                "force": spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32)
-            }
-        )
+        self.action_space = spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float64)
 
     def _get_obs(self):
         """Convert internal state to observation format.
@@ -72,14 +66,33 @@ class CartPoleEnv(gym.Env):
 
         return self._get_obs()
     
-    def step(self):
+    def step(self, action):
 
-        x1 = self._get_obs()["cart_position"]
-        x2 = self._get_obs()["cart_velocity"]
-        x3 = self._get_obs()["pole_angle"] # theta
-        x4 = self._get_obs()["pole_angular_velocity"] # theta_dot
+        force = action # action should be a force applied to cart: -F (left), +F (right), 0 (no force)
+        
+        mp = self.pole_mass
+        M = self.cart_mass
+        L = self.pole_length
+        g = self.gravity
+
+        x1 = self._cart_position # x
+        x2 = self._cart_velocity # x_dot
+        x3 = self._pole_angle # theta
+        x4 = self._pole_angular_velocity #theta_dot
         
         x1_dot= x2
-        x2_dot = -mp * L * np.sin(x3) * np.square(x4) + mp * g * np.cos(x3) * np.sin(x4) + Force / (M + mp * np.square(np.sin(x3)))
+        x2_dot = -mp * L * np.sin(x3) * np.square(x4) + mp * g * np.cos(x3) * np.sin(x4) + force / (M + mp * np.square(np.sin(x3)))
         x3_dot = x4
-        x4_dot = -(M + mp) * g * np.sin(x3) - mp * L * np.sin(x3) * np.cos(x3) * np.square(x4) - Force * np.cos(x3) / (L * (M + mp * np.square(np.sin(x3))) )
+        x4_dot = -(M + mp) * g * np.sin(x3) - mp * L * np.sin(x3) * np.cos(x3) * np.square(x4) - force * np.cos(x3) / (L * (M + mp * np.square(np.sin(x3))) )
+
+        self.update_states()
+        # update position, velocities with x[1-4] dot?
+        x = x + x_dot * delta_time
+        
+        theta = theta + theta_dot * delta_time
+
+        # reward = 1 if self._pole_angle equals 0
+
+        observations = self._get_obs()
+
+        return observations, reward, terminated, False, {}
