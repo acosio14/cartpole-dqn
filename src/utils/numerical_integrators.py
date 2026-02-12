c90ee716c7889423c571e8b9cadb2a659bb69099
Adrian Cosio <adriancosio14@gmail.com>
2026-02-12 07:04:20 -0600

    feat: add eurler and rk4

--- src/utils/numerical_integrators.py
+++ src/utils/numerical_integrators.py
@@ -1,8 +1,15 @@
 
 
-def euler(state, ode, time, timestep):
-    ...
+def euler(df, x, x_dot, timestep):
+    
+    return  x + timestep * x_dot
 
 
-def runge_kutta_fourth_order(state, ode, time, timestep):
-    ...
+
+def runge_kutta_fourth_order(df, state, force, timestep):
+    k1 = df(state, force)
+    k2 = df(state + k1/2, force)
+    k3 = df(state + k2/2, force)
+    k4 = df(state + k3, force)
+
+    return state + (1/6)*timestep*(k1 + 2*k2 + 2*k3 + k4) # state_1 (n+1)
