2c2b477ee529c7f85988a35c447606876e0f6fb3
Adrian Cosio <adriancosio14@gmail.com>
2026-02-15 19:48:42 -0600

    fix: change random action to choose from action_space.

--- src/carpole/dqn_agent/agent.py
+++ src/carpole/dqn_agent/agent.py
@@ -1,4 +1,7 @@
 import numpy as np
+import torch
+from network import DQN
+from envs.cartpole_env import CartPoleEnv
 
 class CartpoleAgent():
     def __init__(self, replay_memory, policy_network, target_network):
@@ -6,15 +9,15 @@ class CartpoleAgent():
         self.policy_network = policy_network
         self.target_network = target_network
         
-    def calculate_q_value(state):
-        q_value = DQN(state).to(device="mps")
+    def calculate_q_value(self, state):
+        return DQN(state).to(device="mps")
 
-    def select_action(epsilon, q_values):
+    def select_action(self, action_space, epsilon, q_values):
         """ Selection action based on epsilon and Q values."""
         
         if np.random.random() < epsilon:
-            action = np.random.randint(low=0, high=1) # low, high placeholders
+            action = np.random.choice(action_space) # action_space = [0, 1, 2, 3, 4] mapped to [-10, -5, 0, 5, 10]
         else:
-            action = np.argmax(q_values)
+            action = torch.argmax(q_values).item()
 
         return action
