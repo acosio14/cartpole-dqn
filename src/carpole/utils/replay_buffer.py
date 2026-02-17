2f74b13a32f3ca0ea05f28cd2864f515fb11b34e
Adrian Cosio <adriancosio14@gmail.com>
2026-02-17 05:34:36 -0600

    feat: add replay buffer inherited from deque, added sampling

--- src/carpole/utils/replay_buffer.py
+++ src/carpole/utils/replay_buffer.py
@@ -1,18 +1,9 @@
+from collections import deque
+import random
 
-
-class ReplayBuffer():
+class ReplayBuffer(deque):
     def __init__(self, max_size):
-        self.memory = []
-        self.max_size = max_size
+        super().__init__(maxlen=max_size)
 
-    def add(self, data: tuple):
-        if len(self.memory) < self.max_size:
-            # just add
-            self.memory.extend(data)
-        else:
-            ...
-            # remove oldest, add newest
-    
     def sample(self, batch_size):
-        ...
-        # shuffle list and return a batch
+        return random.sample(self, min(batch_size, len(self)))
