04b8bc8c2a00338f1855a0fff5208a275a215e61
Adrian Cosio <adriancosio14@gmail.com>
2026-02-17 06:39:53 -0600

    refactor: replace replay_buffer method with imported class.

--- src/carpole/dqn_agent/agent.py
+++ src/carpole/dqn_agent/agent.py
@@ -3,13 +3,25 @@ import torch
 from network import DQN
 import gymnasium as gym
 from carpole.dqn_agent.network import DQN
+from utils.replay_buffer import ReplayBuffer
+from training.trainner import Trainner
+
 
 class CartPoleAgent():
-    def __init__(self, env: gym.Env, policy_network: DQN, target_network: DQN, epsilon: float):
+    def __init__(
+            self,
+            env: gym.Env,
+            policy_network: DQN,
+            target_network: DQN, 
+            epsilon: float,
+            discount_factor: float,
+        ):
         self.env = env
         self.policy_network = policy_network
         self.target_network = target_network
         self.epsilon = epsilon # epsilon = 1 / sqrt(n + 1)
+        self.discount_factor = discount_factor
+        self.replay_buffer = ReplayBuffer(maxlen=10000)
     
     def select_action(self, state):
         """ Selection action based on epsilon and Q values."""
@@ -22,18 +34,21 @@ class CartPoleAgent():
 
         return action
     
-    def replay_buffer(self, state, action, reward, next_state, done):
-        # store (state, action, reward, next state, done)
-        # support random batching
-        ...
-
-    def learn(self, batch_size, gamma):
+    def learn(self, state, action, reward, next_state, terminated):
         # Sample a batch from replay_buffer
         # Compute predicted Q-values for the states and actions in the batch using policy_network
         # Compute Bellmans targets using target_network
         # Compute Loss (prediceted Q and target diff)
         # Backpropogate to update polic_network weights
-        ...
+        self.replay_buffer.append(state, action, reward, next_state, terminated)
+        
+        if self.replay_buffer.is_full:
+            mini_batch = self.replay_buffer.sample(batch_size=10)
+
+            
+
+
+        self.update_target_network()
 
     def update_target_network(self):
         # should copy policy network weights to target_network
