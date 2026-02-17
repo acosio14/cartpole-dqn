22c4c882be94d1ce614f8cffa7225739c6d094e5
Adrian Cosio <adriancosio14@gmail.com>
2026-02-17 06:38:56 -0600

    feat: add method to check if replay_buffer is full and return True

--- src/carpole/utils/replay_buffer.py
+++ src/carpole/utils/replay_buffer.py
@@ -4,9 +4,15 @@ import random
 class ReplayBuffer(deque):
     def __init__(self, maxlen):
         super().__init__(maxlen=maxlen)
+        self.maxlen = maxlen
+        self._is_full = False
 
     def sample(self, batch_size):
         if len(self) == 0:
             return ValueError("Replay Buffer is empty")
         
         return random.sample(self, min(batch_size, len(self)))
+    
+    def _is_maxlen(self):
+        if len(self) == self.maxlen:
+            self._is_full = True
