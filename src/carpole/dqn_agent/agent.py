cf7ad48821b8020a006a39c6ebfbb75f93e1cba0
Adrian Cosio <adriancosio14@gmail.com>
2026-02-18 21:06:22 -0600

    refactor: remove various functions outside into train loop

--- src/carpole/dqn_agent/agent.py
+++ src/carpole/dqn_agent/agent.py
@@ -12,6 +12,7 @@ class CartPoleAgent():
             env: gym.Env,
             policy_network: DQN,
             target_network: DQN, 
+            learning_rate: float,
             epsilon: float,
             discount_factor: float,
             maxlen: int = 10000,
@@ -19,11 +20,10 @@ class CartPoleAgent():
         self.env = env
         self.policy_network = policy_network
         self.target_network = target_network
-        self.epsilon = epsilon # epsilon = 1 / sqrt(n + 1)
+        self.epsilon = epsilon
         self.discount_factor = discount_factor
         self.replay_buffer = ReplayBuffer(maxlen)
-        self.iteration = 0
-        self.learning_rate = 0.001
+        self.learning_rate = learning_rate
     
     def select_action(self, state):
         """ Selection action based on epsilon and Q values."""
@@ -36,43 +36,34 @@ class CartPoleAgent():
 
         return action
 
-    def learn(self, state, action, reward, next_state, terminated, batch_size):
+    def update(self, state, action, reward, next_state, terminated):
         # Sample a batch from replay_buffer
         # Compute predicted Q-values for the states and actions in the batch using policy_network
         # Compute Bellmans targets using target_network
         # Compute Loss (prediceted Q and target diff)
         # Backpropogate to update polic_network weights
         # optimizer step
-        self.replay_buffer.append(state, action, reward, next_state, terminated)
-        # DON"T KNOW IF THIS SHOULD BE OUTSIDE IN TRAINING LOOP
-        ''' Only bring in one batch?'''
         
-        if self.replay_buffer.is_full:
-            mini_batch = self.replay_buffer.sample(batch_size)
-            state, action, reward, next_state, terminated = mini_batch
-            # target -> shape(B,A) B=batch_size A=actions
-            # max(dimension = 1) - take max along dimension 1 (actions)
-            # (value, indicies) -> [0] = only values
-            max_q_values = self.target_network(next_state).max(1)[0] # A batch of values
-            target_q_values = reward + self.discount_factor * max_q_values * (1 - terminated)
-            q_values =  self.policy_network(state)
+        # target -> shape(B,A) B=batch_size A=actions
+        # max(dimension = 1) - take max along dimension 1 (actions)
+        # (value, indicies) -> [0] = only values
+        max_q_values = self.target_network(next_state).max(1)[0] # A batch of values
+        target_q_values = reward + self.discount_factor * max_q_values * (1 - terminated)
+        q_values =  self.policy_network(state)
 
-            loss = F.mse_loss(q_values, target_q_values)
-            loss.backward()
-            torch.optim.Adam(
-                params=self.policy_network.parameters(), 
-                rl=self.learning_rate,
-            ).step()
-        
-        self.update_target_network(update_frequency=60)
+        loss = F.mse_loss(q_values, target_q_values)
+        loss.backward()
+        torch.optim.Adam(
+            params=self.policy_network.parameters(), 
+            rl=self.learning_rate,
+        ).step()
 
 
-    def update_target_network(self, update_frequency):
+    def update_target_network(self, steps, update_frequency):
         # should copy policy network weights to target_network
         # need to decide how frequent (every N steps)
-        if self.iteration % update_frequency == 0:
+        if steps % update_frequency == 0:
             self.target_network.load_state_dict(self.policy_network.state_dict())
-            self.iteration = 0
 
     def decay_epsilon(self, episode):
         self.epsilon = (1 / np.sqrt(episode + 1)) * self.epsilon
