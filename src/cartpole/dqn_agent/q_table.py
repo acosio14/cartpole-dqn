a6b32215a885b17a493351733606ed421d17b5b5
Adrian Cosio <adriancosio14@gmail.com>
2026-02-20 05:09:34 -0600

    fix: fix namming error for cartpole.

---
+++ src/cartpole/dqn_agent/q_table.py
@@ -0,0 +1,10 @@
+
+class QTable():
+    def __init__(self, state, action, discount_factor):
+        self.state = state
+        self.action = action
+        self.discount_factor = discount_factor
+        
+
+    def update_q_table():
+        ...
