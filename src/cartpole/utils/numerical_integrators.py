70927a5617b2fc24888d0693177ba92f19b7546d
Adrian Cosio <adriancosio14@gmail.com>
2026-02-21 19:54:48 -0600

    fix: add ode as import and modify rk4 function.

--- src/cartpole/utils/numerical_integrators.py
+++ src/cartpole/utils/numerical_integrators.py
@@ -1,15 +1,16 @@
 
+from envs.cartpole_ode import ordinay_differantial_equation as df
 
-def euler(df, x, x_dot, timestep):
+def euler(x, x_dot, timestep):
     
     return  x + timestep * x_dot
 
 
-
-def runge_kutta_fourth_order(df, state, force, timestep):
-    k1 = df(state, force)
-    k2 = df(state + k1/2, force)
-    k3 = df(state + k2/2, force)
-    k4 = df(state + k3, force)
+# Can't import df as ode because its a method of cartpoleenv
+def runge_kutta_fourth_order(state, force, timestep, constants):
+    k1 = df(state, force, constants)
+    k2 = df(state + k1/2, force, constants)
+    k3 = df(state + k2/2, force, constants)
+    k4 = df(state + k3, force, constants)
 
     return state + (1/6)*timestep*(k1 + 2*k2 + 2*k3 + k4) # state_1 (n+1)
