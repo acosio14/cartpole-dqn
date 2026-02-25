fix: modify ode to be numerically stable and correct.

src/cartpole/envs/cartpole_ode.py
@@ -2,23 +2,14 @@ import numpy as np
 
 def ordinay_differantial_equation(state, force, constants):
     
-    mp = constants[0]
-    M = constants[1]
-    L = constants[2]
-    g = constants[3]
+    mp, M, L, g = constants
+
+    x, x_dot, theta, theta_dot = state
     
-    # Get Derivatives
-    x_dot= state[1] # cart velocity
-    x_ddot = (
-        -mp * L * np.sin(state[2]) * np.square(state[3])
-        + mp * g * np.cos(state[2]) * np.sin(state[3])
-        + force / (M + mp * np.square(np.sin(state[2]))) 
-    ) # cart acceleration
-    theta_dot = state[3] # angular velocity
-    theta_ddot = (
-        -(M + mp) * g * np.sin(state[2]) 
-        - mp * L * np.sin(state[2]) * np.cos(state[2]) * np.square(state[3]) 
-        - force * np.cos(state[2]) / (L * (M + mp * np.square(np.sin(state[2]))) ) 
-    )# angular acceleration
+    temp = (force + mp*L*theta_dot**2*np.sin(theta)) / (M+mp)
+    
+    theta_ddot = (g*np.sin(theta) - np.cos(theta)*temp) / (L*(4/3 - mp*np.cos(theta)**2/(M+mp)))
+    
+    x_ddot = temp - (mp*L*theta_ddot*np.cos(theta))/(M+mp)
 
     return np.array([x_dot, x_ddot, theta_dot, theta_ddot])