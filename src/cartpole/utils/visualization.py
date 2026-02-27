48e6985ebb5565714b824893f9db01b7ddda5a83
Adrian Cosio <adriancosio14@gmail.com>
2026-02-27 05:52:02 -0600

    feat: add moving average plots

--- src/cartpole/utils/visualization.py
+++ src/cartpole/utils/visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass
 class RLPlots:
@@ -13,18 +14,41 @@ class RLPlots:
         plt.xlabel("Episode")
         plt.ylabel("Reward")
         plt.title("Rewards Per Episode")
+    
+    def plot_learning_curve_moving_avg(self, window_size):
+        avg_rewards = np.convolve(self.rewards, np.ones(window_size)/window_size, mode='valid')
+        plt.plot(avg_rewards)
+        plt.xlabel("Episode")
+        plt.ylabel("Avg Reward")
+        plt.title("Avg Rewards Per Episode")
 
+    # Steps
     def plot_steps_per_episode(self):
         plt.plot(self.steps)
         plt.xlabel("Episode")
         plt.ylabel("Steps")
         plt.title("Steps Per Episode")
 
+    def plot_step_moving_avg(self, window_size):
+        avg_steps = np.convolve(self.steps, np.ones(window_size)/window_size, mode='valid')
+        plt.plot(avg_steps)
+        plt.xlabel("Episode")
+        plt.ylabel("Avg Steps")
+        plt.title("Avg Steps Per Episode")
+
+    # Losses
     def plot_mse_loss(self):
         plt.plot(self.losses)
         plt.xlabel("Episode")
         plt.ylabel("Loss")
         plt.title("Loss Per Episode")
+
+    def plot_loss_moving_avg(self, window_size):
+        avg_loss = np.convolve(self.losses, np.ones(window_size)/window_size, mode='valid')
+        plt.plot(avg_loss)
+        plt.xlabel("Episode")
+        plt.ylabel("Avg Loss")
+        plt.title("Avg Loss Per Episode")
 
     def plot_epsilon(self):
         plt.plot(self.epsilons)
